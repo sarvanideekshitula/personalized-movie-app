cda72fd857d667841bdeb855342219c9fdbeb9c7
sarvanideekshitula <sarvanimini@gmail.com>
2020-02-04 13:14:21 +0530

    Implemented api to get movie details from id

--- src/handlers/externalApiHandler.js
+++ src/handlers/externalApiHandler.js
@@ -3,9 +3,7 @@ const axios = require('axios').default;
 
 const fetchData = async (request, h)=> {
 	try{
-		// console.log('Entered handlers');
 		let data = request.payload;
-		// console.log(data.apis[0]);
 		const moviesData = await axios.get(data.apis[0]);
 		const genresData = await axios.get(data.apis[1]);
 		const actorsData = await axios.get(data.apis[2]);
@@ -20,4 +18,29 @@ const fetchData = async (request, h)=> {
 	}
 };
 
-module.exports = {fetchData};
+const getMovieData = async(request, h) => {
+	try{
+		let result = {};
+		let movieId = request.params.id;
+		const movieDetails = await dbUtils.findOneMovie(movieId);
+		result.name = movieDetails.name;
+		console.log(movieDetails.genres);
+		const genreDetails = await dbUtils.findAllGenres(movieDetails.genres);
+		result.genres = genreDetails;
+		const actorDetails = await dbUtils.findAllActors();
+		let actorsData = [];
+		actorDetails.forEach(entry => {
+			if(entry.movies.includes(movieId)){
+				actorsData.push(entry.name);
+			}
+		});
+		result.actors = actorsData;
+		return h.response(result).code(200);
+	}
+	catch(e){
+		console.log(e);
+		return h.response(e.message).code(500);
+	}
+};
+
+module.exports = {fetchData, getMovieData};
